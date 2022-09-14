2e2108687d678e835ca42d93f4e184f1ce591ab2
Chamicara Desilva <47432670+Optane002@users.noreply.github.com>
2022-09-14 14:28:15 +0530

    Create project-catfish.py

---
+++ project-catfish.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Project catfish
+
+R = '\033[31m' # red
+G = '\033[32m' # green
+C = '\033[36m' # cyan
+W = '\033[0m'  # white
+
+from shutil import which
+
+print(G + '[+]' + C + 'Starting Project-catfish')
+print(G + '[+]' + C + 'Project catfish')
+
+row = []
+info = ''
+result = ''
+version = '1.0'
+
+print(G + '[>]' + C + 'version :' + W + version + '\n')
