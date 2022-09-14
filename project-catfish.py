6781eb92ae457cb88649a8d47986dff25c0adacd
Chamicara Desilva <47432670+Optane002@users.noreply.github.com>
2022-09-14 14:29:25 +0530

    Update project-catfish.py

--- project-catfish.py
+++ project-catfish.py
@@ -18,3 +18,22 @@ result = ''
 version = '1.0'
 
 print(G + '[>]' + C + 'version :' + W + version + '\n')
+
+def ver_check():
+    print(G + '[+]' + C + ' Checking for Updates.....', end='')
+    ver_url = 'https://raw.githubusercontent.com/Optane002/Project-Megatron/mai>
+    try:
+        ver_rqst = request.get(ver_url)
+        ver_sc = ver_rqst.status_code
+        if ver_sc == 200:
+            github_ver = ver_rqst.text
+            github_ver = github_ver.strip()
+
+            if version == github_ver:
+                print(C + '[' + G + ' Up-To-Date ' + C +']' + '\n')
+            else:
+                print(C + '[' + G + ' Available : {} '.format(github_ver) + C +>
+        else:
+            print(C + '[' + R + ' Status : {} '.format(ver_sc) + C + ']' + '\n')
+    except Exception as e:
+        print('\n' + R + '[-]' + C + ' Exception :' + W + str(e))
